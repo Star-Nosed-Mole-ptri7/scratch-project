6e032539519f17e834bf035e00f146536ec99042
Mark Alexander <markalexander@example.com>
2022-06-29 19:10:08 -0700

    Connected test database

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
+// require('dotenv').config(); // For .env
+
 const express = require('express');
 const app = express();
 const path = require('path');
+
+const dbRouter = require('./routes/dbRouter');
+
+app.use(express.json());
 
 app.get('/build/bundle.js', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '..', 'build', 'bundle.js'));
@@ -9,5 +15,7 @@ app.get('/build/bundle.js', (req, res) => {
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '..', 'client', 'index.html'));
 });
+
+app.use('/api', dbRouter);
 
 app.listen(3000);
