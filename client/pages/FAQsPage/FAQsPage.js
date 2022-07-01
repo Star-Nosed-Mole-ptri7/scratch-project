9da87842bf3d3096be98784653af06a2eba1f9e3
Mark Alexander <markalexander@example.com>
2022-06-30 19:43:18 -0700

    Created basic mockup for FAQ page

---
+++ client/pages/FAQsPage/FAQsPage.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import jsonFAQ from './faq.json';
+import './FAQsPage.css'
+
+function FAQsPage(props) {
+  const faqItems = [];
+  jsonFAQ.forEach(item => {
+    faqItems.push(
+      <>
+      <details>
+        <summary className="faqQuestion">{item.question}</summary>
+        {item.answer}
+      </details>
+      </>
+    );
+  });
+  return (
+    <div>
+      <h1>FAQ</h1>
+      {faqItems}
+    </div>
+  );
+}
+
+export default FAQsPage;
