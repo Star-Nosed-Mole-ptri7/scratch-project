dd61ff2e680de63781ded0b179e20c29c03d3808
Mark Alexander <markalexander@example.com>
2022-07-05 19:12:05 -0700

    Connected news api to articles page

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,14 @@ module.exports = {
           }
         }
       },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //     },
+      //   ],
+      // },
       {
         test: /\.css/,
         exclude: /node_modules/,
@@ -43,5 +43,6 @@ module.exports = {
   })],
   devServer: {
     historyApiFallback: true
-  }
+  },
+  devtool: 'inline-source-map'
 };
