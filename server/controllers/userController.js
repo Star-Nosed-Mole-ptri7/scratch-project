6e2879f58a1f898f5808867e1348cbf5ad0c5796
jessdvila <jdav92@gmail.com>
2022-07-08 19:24:04 -0400

    Updated usercontrollers jess

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,19 @@
 const { Pool } = require('pg');
 
+//NEED TO INSTALL & REQUIRE BCRYPT//
+
 const pool = new Pool({
   connectionString: "postgres://otuxrvdv:Q1-zrxVqaZ9l9P2EGT6IxS2SsH2Frp_p@heffalump.db.elephantsql.com/otuxrvdv",
 });
 
 const userController = {};
 
-userController.getTest = (req, res, next) => {
-  pool.query('SELECT * FROM "user"')
+
+
+userController.getUser = (req, res, next) => {
+
+  pool.query(`SELECT * FROM "user" 
+            WHERE user_name = '${req.params.name}'`) //template literal isn't working?
     .then((data) => {
       console.log(data.rows);
       res.locals = data.rows;
@@ -18,29 +24,52 @@ userController.getTest = (req, res, next) => {
     });
 };
 
-userController.createUser = (req, res, next) => {
+// userController.createUser = (req, res, next) => {
 
-  const {user_id, first_name, last_name, user_name, password, location} = req.body;
-  const values = [user_id, first_name, last_name, user_name, password, location];
-  const text = 'INSERT INTO "user" (user_id, first_name, last_name, user_name, password, location)' +
-                `VALUES ($1, $2, $3, $4, $5, $6)`;
+//   const {first_name, last_name, user_name, password, recycle_progress} = req.body;
+//   const hash = bcrypt.hash(password, 10);
+//   const values = [first_name, last_name, user_name, password, recycle_progress];
+//   const text = `INSERT INTO "user" (first_name, last_name, user_name, ${hash}, recycle_progress)` +
+//                 `VALUES ($1, $2, $3, $4, $5, $6)`;
 
-  pool.query(text, values)
-    .then((data) => {
-      res.locals = data.rows;
-      return next();
-    })
-    .catch((err) => {
-      return next(console.log(err));
-    });
-};
+//   pool.query(text, values)
+//     .then((data) => {
+//       res.locals = data.rows;
+//       return next();
+//     })
+//     .catch((err) => {
+//       return next(console.log(err));
+//     });
+// };
+
+//   const {user_name, password} = req.body;
+//   const values = [user_name, password];
+//   const text = `SELECT user_name, password FROM "user" 
+//                 WHERE user_name = ${req.params.name}`;
+//   pool.query(text, values)
+//     .then((data) => {
+//       if(user_name) {
+//         const validPass = bcrypt.compare(password, user.password);
+//         if(validPass) {
+//           return next ();
+//         }else {
+//           return next ({message: 'Wrong password!'});
+//         }
+//       } 
+     
+//     })
+//     .catch((err) => {
+//       return next(console.log(err));
+//     });
 
 // userController.updateUser = (req, res, next) => {
-//   // pool.query('SELECT * FROM algos ORDER BY id')
-//   // pool.query('SELECT * FROM "user"')
 
-//   //username, pass 
-//   //location, recycle progress
+// const {password} = req.body;
+// const values = [password];
+// const text = `UPDATE "user"
+//               SET password = ${password}
+//               WHERE user_name = ${req.params.name}`
+// pool.query(text, values)
 //     .then((data) => {
 //       console.log(data.rows);
 //       res.locals = data.rows;
@@ -52,17 +81,19 @@ userController.createUser = (req, res, next) => {
 // };
 
 
-userController.deleteUser = (req, res, next) => {
+// userController.deleteUser = (req, res, next) => {
 
-  const text = `DELETE FROM "user" WHERE user_id = ${req.params.id} `
-  pool.query(text)
-    .then((data) => {
-      if(!data) return next ({message: 'User cannot be deleted'}) 
-      return next();
-    })
-    .catch((err) => {
-      return next(console.log(err));
-    });
-};
+//   const text = `DELETE FROM "user" WHERE user_name = ${req.params.name} `
+//   pool.query(text)
+//     .then((data) => {
+//       if(!data) return next ({message: 'User cannot be deleted'}) 
+//       return next();
+//     })
+//     .catch((err) => {
+//       return next(console.log(err));
+//     });
+// };
+
+
 
 module.exports = userController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,31 +4,27 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
-router.get('/', userController.getTest, (req, res) => {
+router.get('/:name', userController.getUser, (req, res) => {
   return res.status(200).send(res.locals);
 });
 
-router.post('/', userController.createUser, (req, res) => {
-  return res.status(200).send(res.locals)
-})
-
-// router.patch('/', userController.editUser, (req, res) => {
+// router.post('/', userController.createUser, (req, res) => {
 //   return res.status(200).send(res.locals)
 // })
 
-router.delete('/:id', userController.deleteUser, (req, res) => {
-  return res.status(200).send(res.locals)
-})
+// router.patch('/', userController.updateUser, (req, res) => {
+//   return res.status(200).send(res.locals)
+// })
 
+// router.delete('/:name', userController.deleteUser, (req, res) => {
+//   return res.status(200).send(res.locals)
 
+// })
 
+// router.post('/login', userController.verifyUser, (req, res) => {
+//   // resturn res.status(200).sendFile(path.join(__dirname, **SERVE USER PAGE**)
 
-// router.post('/', dbController.saveAlgo, (req, res) => {
-//   return res.status(200).send(res.locals);
 // });
 
-// router.delete('/', dbController.delAlgo, (req, res) => {
-//   return res.status(200).send(res.locals);
-// });
 
 module.exports = router;
