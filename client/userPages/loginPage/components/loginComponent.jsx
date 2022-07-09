e4e4602fe6bcf346829cf7302dafaf36e44b08a1
Mark Alexander <markalexander@example.com>
2022-07-09 12:19:19 -0700

    Styling started for login and signup

--- client/userPages/loginPage/components/loginComponent.jsx
+++ client/userPages/loginPage/components/loginComponent.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
+// import '../login.css';
+
 function LoginComponent() {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
