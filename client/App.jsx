ebd0af4af5efba0196b4a1e7a08490d9968407b1
Erika Collins <erikasusancollins@gmail.comclear>
2022-07-16 13:27:17 -0600

    clean merge with Saif, errors handled

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,5 @@
-import carbonFootprint from "./CarbonFootprint.jsx";
-import CarbonOptions from "./CarbonOptions.jsx";
+import carbonFootprint from "./components/CarbonFootprint.jsx";
+import CarbonOptions from "./components/CarbonOptions.jsx";
 import React, { useState } from "react";
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ButtonAppBar from "./components/Nav.jsx";
--- client/components/CarbonOptions.jsx
+++ client/components/CarbonOptions.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './style.css'; //test this
+import '../style.css'; //test this
 import { Link } from 'react-router-dom';
 
 const CarbonOptions = () => {
