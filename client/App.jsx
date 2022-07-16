9967e1639a9cc80d0d7ffcd19e99c23ece5f3b35
saifbeiruty <saif.beiruty@mail.utoronto.ca>
2022-07-16 14:50:12 -0400

    homepage inprogress

---
+++ client/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import ButtonAppBar from "./components/Nav.jsx";
+import Body from "./components/Body.jsx";
+import SignUp from "./components/Signup.jsx";
+import SignIn from "./components/Login.jsx";
+
+
+const App = () => {
+    const [loggedIn, setLoggedIn] = useState(false)
+
+    if (loggedIn) {
+
+    }
+
+    return (
+        <div>
+            <Routes>
+                <Route path='/' element={<ButtonAppBar />}>
+                    <Route path='/' element={<Body />}>
+                        <Route path='/Signup' element={<SignUp />}>
+                        </Route>
+                        <Route path='/Login' element={<SignIn />}>
+                        </Route>
+                    </Route>
+                </Route>
+            </Routes>
+        </div>
+    )
+}
+
+export default App;
