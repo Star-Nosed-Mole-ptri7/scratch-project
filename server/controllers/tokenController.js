25f5ca780856c9f36c842669c7d24696d7e1457f
ethanmcrae <ethanmcrae0@gmail.com>
2022-07-17 09:54:48 -0600

    added social posts and logout functionality

--- server/controllers/tokenController.js
+++ server/controllers/tokenController.js
@@ -1,6 +1,25 @@
 const jwt = require('jsonwebtoken');
+const jwtDecode = require('jwt-decode');
 
 const tokenController = {};
+
+tokenController.createToken = (req, res, next) => {
+  // Destructure "remember me" from the request's body.
+  const { rememberMe } = req.body;
+
+  // Create a jwt signed token upon successful login
+  const token = jwt.sign(
+    { userData: res.locals.userData }, // User info (user_id, email, hashed password, name_first, name_last, created at)
+    process.env.ACCESS_TOKEN_SECRET, // Secret for all of our tokens
+    { expiresIn: rememberMe ? '1d' : '10m' } // Token expiration depends on rememberMe
+  );
+
+  // Create a cookie to be stored on the client with the token
+  res.cookie('token', token, { httpOnly: true });
+
+  // Continue to next middleware function
+  return next();
+}
 
 tokenController.verifyToken = (req, res, next) => {
   // Destructure the jwt token from the client's cookies
@@ -15,5 +34,19 @@ tokenController.verifyToken = (req, res, next) => {
     return err ? res.status(403).send('Invalid Token') : next();
   });
 };
+
+tokenController.decodeToken = (req, res, next) => {
+  // Destructure the jwt token from the client's cookies
+  const { token } = req.cookies;
+
+  // Use jwt-decode to decode our token
+  const { userData } = jwtDecode(token);
+
+  // Save user data to be used by preceeding middleware functions
+  res.locals.userData = userData;
+
+  // Continue to next middleware function
+  return next();
+}
 
 module.exports = tokenController;
